255b07128c51ae0ddae5af3ed48cf7caa8f29b1b
Alecsandro <alex247417@hotmail.com>
2025-12-16 14:01:51 -0300

    Feat: Exercício de fixação sobre Vetores (Pensionato)

---
+++ Seção_6/Exercício de fixação Vetores/Exercício de fixação Vetores/BedRooms.cs
@@ -0,0 +1,21 @@
+﻿using System.Runtime;
+
+namespace Exercício_de_fixação_Vetores;
+
+public class BedRooms
+{
+     public string Name { get;  set; }
+     public string Email { get;  set; }
+
+     public BedRooms(string name, string email)
+     {
+      Name = name;
+      Email = email;          
+     }
+
+     public override string ToString()
+     {
+         return Name + ", " + Email;
+     }
+
+}
---
+++ Seção_6/Exercício de fixação Vetores/Exercício de fixação Vetores/Program.cs
@@ -0,0 +1,36 @@
+﻿using Exercício_de_fixação_Vetores;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        BedRooms[] rent = new BedRooms[10];
+        
+        Console.Write("Quantos quartos serão alugados? ");
+        int Number = int.Parse(Console.ReadLine());
+       
+        for (int i = 0; i < Number; i++)
+        {
+            Console.WriteLine();
+            Console.WriteLine($"Aluguel #{i}:");
+            Console.WriteLine("Qual Nome  do aluno?");
+            string name = Console.ReadLine();
+            Console.WriteLine("Qual Email  do aluno?");
+            string email = Console.ReadLine();
+            Console.WriteLine("Qual Quarto Deseja alugar:");
+            int room = int.Parse(Console.ReadLine());
+            rent[room] = new BedRooms(name, email);
+            
+        }
+
+        Console.WriteLine("Busy rooms:");
+
+        for (int i = 0; i < 10; i++)
+        {
+            if (rent[i] != null)
+            {
+                Console.WriteLine(i + ": " + rent[i]);
+            }
+        }
+    }
+}
