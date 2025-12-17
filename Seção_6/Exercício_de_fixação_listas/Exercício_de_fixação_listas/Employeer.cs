ce49e36df2b727c5a72195bc5d5d0b5f7780ed27
Alecsandro <alex247417@hotmail.com>
2025-12-17 14:57:15 -0300

    Feat: Recuperando exercicio listas

---
+++ Seção_6/Exercício_de_fixação_listas/Exercício_de_fixação_listas/Employeer.cs
@@ -0,0 +1,25 @@
+﻿namespace Exercício_de_fixação_listas;
+
+public class Employeer
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public decimal Salary { get;  private set; }
+    
+    public Employeer(int id, string name, decimal salary)
+    {
+        Id = id;
+        Name = name;
+        Salary = salary;
+    }
+
+    public void Increase(int percentage)
+    {
+        Salary += Salary * percentage / 100m;
+    }
+
+    public override string ToString()
+    {
+        return $"Id: {Id}, Name: {Name}, Salary: {Salary.ToString("F2")}";
+    }
+}
---
+++ Seção_6/Exercício_de_fixação_listas/Exercício_de_fixação_listas/Program.cs
@@ -0,0 +1,50 @@
+﻿using Exercício_de_fixação_listas;
+class Program
+{
+    static void Main(string[] args)
+    {
+        // criando lista:
+        List<Employeer> lista = new List<Employeer>();
+        
+        // ler os dados do funcionarios 
+        Console.Write("Digite Quantidade de funcionarios: ");
+        int n = int.Parse(Console.ReadLine());
+        for (int i = 0; i < n; i++)
+        {
+            Console.WriteLine("Id:");
+            int id = int.Parse(Console.ReadLine());
+            Console.WriteLine("Nome:");
+            string nome = Console.ReadLine();
+            Console.WriteLine("Salario:");
+            decimal salary = decimal.Parse(Console.ReadLine());
+            Employeer funcionario = new Employeer(id, nome, salary);
+            lista.Add(funcionario);
+        }
+        foreach (var VARIABLE in lista)
+        {
+            Console.WriteLine($"Employee#{lista}:");
+            Console.WriteLine(VARIABLE);
+        }
+        Console.WriteLine("Enter the employee Id that will have salary increase : ");
+        int searchId = int.Parse(Console.ReadLine());
+        Employeer emp = lista.Find(x => x.Id == searchId);
+        if (emp != null)
+        {
+            Console.WriteLine("Enter the percentage:");
+            int percentage = int.Parse(Console.ReadLine());
+            emp.Increase(percentage);
+        }
+        else
+        {
+            Console.WriteLine("Employee ID not found");
+        }
+        Console.WriteLine("\n Updated List of employees:");
+        foreach (Employeer e in lista)
+        {
+            Console.WriteLine(e);
+        }
+        
+        Console.ReadKey();
+
+    }
+}
