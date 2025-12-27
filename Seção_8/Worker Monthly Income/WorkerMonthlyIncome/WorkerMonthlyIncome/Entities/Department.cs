8a8c01ae196836ebe657be09f1a2f2fc2183327e
Alecsandro <alex247417@hotmail.com>
2025-12-27 18:37:46 -0300

    Feat: Implementação da lógica principal de cadastro de Trabalhador e Contratos
    Criação da interação via Console para instanciar o Worker, o Department e adicionar múltiplos contratos à lista do trabalhador.

---
+++ Seção_8/Worker Monthly Income/WorkerMonthlyIncome/WorkerMonthlyIncome/Entities/Department.cs
@@ -0,0 +1,15 @@
+﻿namespace WorkerMonthlyIncome.Entities;
+
+public class Department
+{
+    public String Name { get; set; }
+
+    public Department()
+    {
+    }
+
+    public Department(string name)
+    {
+        Name = name;
+    }
+}
